0836350d1a9e010999942a38a5942a1b4b3f27e7
Akshaybagai52 <akshaybagai52@gmail.com>
2024-10-27 19:08:38 +0530

    feat: added top bar tabs

--- expo-first/app/(tabs)/explore.tsx
+++ expo-first/app/(tabs)/explore.tsx
@@ -1,12 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
 
 const Explore = () => {
   return (
     <View>
-      <Text>explore</Text>
+      <Link href={"/accountInfo"}>
+        <Text>explore</Text>
+      </Link>
     </View>
-  )
-}
+  );
+};
 
-export default Explore
+export default Explore;
--- expo-first/app/(tabs)/index.tsx
+++ expo-first/app/(tabs)/index.tsx
@@ -1,12 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Suggested from './suggested';
+import Library from './library';
+import Liked from './liked';
 
-const Index = () => {
+const Tab = createMaterialTopTabNavigator();
+
+export default function MyTabs() {
   return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
+    <Tab.Navigator>
+      <Tab.Screen name="Library" component={Library} />
+      <Tab.Screen name="Liked" component={Liked} />
+      <Tab.Screen name="Suggested" component={Suggested} />
+    </Tab.Navigator>
+  );
 }
-
-export default Index
